0182c3bce5c1651c2a1bf7326d5e29c9d21192f7
Abdullah Karacabey <abdullahkaracabey@gmail.com>
2022-05-20 15:30:49 +0300

    Windows example project added

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <mac_address/mac_address_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  MacAddressPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MacAddressPlugin"));
+}
